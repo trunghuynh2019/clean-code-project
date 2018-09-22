6532cd525ea87a2384962d84f739c974244aca4f
Nhat Tran <nhatb1400710@student.ctu.edu.vn>
2018-09-22 19:40:37 +0700

    Add shool class, fuction: insert, show School

---
+++ Nhat_Buoi1/src/nhat_buoi1/School.java
@@ -0,0 +1,22 @@
+package nhat_buoi1;
+
+public class School {
+	private String name;
+
+	public School() {
+		this.name = "";
+	}
+	
+	public School(String name) {
+		
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
