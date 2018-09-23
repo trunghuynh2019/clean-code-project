efe1333f619b3a77edf29c2c14fbd709af334b51
Nhat Tran <nhatb1400710@student.ctu.edu.vn>
2018-09-23 13:09:40 +0700

    Add attribute Teacher List to class School, edit contructor, getter and setter

--- School Management Application/src/School_Management_Application/School.java
+++ School Management Application/src/School_Management_Application/School.java
@@ -1,22 +1,40 @@
 package School_Management_Application;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class School {
 	private String name;
-
+        private ArrayList<Teacher> teacherList;
+        
 	public School() {
-		this.name = "";
+            this.name = "";
+            teacherList = new ArrayList<Teacher>();
 	}
 	
-	public School(String name) {
+	public School(String name, ArrayList<Teacher> teacherList) {
 		
-		this.name = name;
+            this.name = name;
+            this.teacherList = teacherList;
 	}
 	
 	public String getName() {
-		return name;
+            
+            return name;
 	}
 
 	public void setName(String name) {
-		this.name = name;
+            
+            this.name = name;
 	}
+        
+        public ArrayList<Teacher> getTeacherList(){
+            
+            return teacherList;
+        }
+        
+        public void setTeacherList(ArrayList<Teacher> teacherList){
+            
+            this.teacherList = teacherList;
+        }
 }
