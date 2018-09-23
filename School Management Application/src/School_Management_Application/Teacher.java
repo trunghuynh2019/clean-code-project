32f00a863529536b930bf1af752d4a068a9b4590
Nhat Tran <nhatb1400710@student.ctu.edu.vn>
2018-09-23 12:50:39 +0700

    add attribute, contruction, setter and getter

--- School Management Application/src/School_Management_Application/Teacher.java
+++ School Management Application/src/School_Management_Application/Teacher.java
@@ -3,4 +3,36 @@ package School_Management_Application;
 public class Teacher {
     private String name;
     private String address;
+    
+    public Teacher() {
+
+            this.name = "";
+            this.address = "";
+    }
+
+    public Teacher (String name, String address) {
+
+            this.name = name;
+            this.address = address;
+    }
+
+    public String getName() {
+
+        return name;
+    }
+
+    public void setName(String name) {
+
+        this.name = name;
+    }
+
+    public String getAddress(){
+
+        return address;
+    }
+
+    public void setAddress(String address){
+
+        this.address = address;
+    }
 }
