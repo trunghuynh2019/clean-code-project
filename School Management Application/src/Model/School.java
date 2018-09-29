4f91f622396fc1a9fe5c0b9de701fa49aeff68ad
Nhat Tran <nhatb1400710@student.ctu.edu.vn>
2018-09-29 09:50:52 +0700

    Add class TeacherList to save list of Teacher

--- School Management Application/src/Model/School.java
+++ School Management Application/src/Model/School.java
@@ -1,86 +1,89 @@
 package Model;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-
 public class School {  
-        private Integer ID = 0;
-	private String name;
-        private String address;
-        private Integer countStudent;
-        private Integer countTeacher;
-        private ArrayList<Teacher> teacherList;
-        
-	public School() {
-            ID++;
-            name = "";
-            address = "";
-            countStudent = 0;
-            countTeacher = 0;
-            teacherList = new ArrayList<Teacher>();
-	}
-	
-	public School(String name, String address, Integer countStudent, ArrayList<Teacher> teacherList) {
-            ID++;
-            this.name = name;
-            this.address = address;
-            this.countStudent = countStudent;
-            this.teacherList = teacherList;
-            this.countStudent = teacherList.size();
-	}
-        
-        public Integer getID(){
-            
-            return ID;
-        }
-	
-	public String getName() {
-            
-            return name;
-	}
+    private Integer ID = 0;
+    private String name;
+    private String address;
+    private Integer countStudent;
+    private Integer countTeacher;
+    private TeacherList teacherList;
 
-	public void setName(String name) {
-            
-            this.name = name;
-	}
+    public School() {
+        ID++;
+        name = "";
+        address = "";
+        countStudent = 0;
+        countTeacher = 0;
+        teacherList = new TeacherList();
+    }
+
+    public School(String name, String address, Integer countStudent, TeacherList teacherList) {
+        ID++;
+        this.name = name;
+        this.address = address;
+        this.countStudent = countStudent;
+        this.teacherList = teacherList;
+        this.countTeacher = teacherList.getSize();
+    }
+
+    public Integer getID(){
+
+        return ID;
+    }
+
+    public String getName() {
+
+        return name;
+    }
+
+    public void setName(String name) {
+
+        this.name = name;
+    }
+
+    public String getAddress(){
+
+        return address;
+    }
+
+    public void setAddress(String address){
+
+        this.address = address;
+    }
+
+    public Integer getCountStudent(){
+
+        return countStudent;
+    }
+
+    public void setCountStudent(Integer countStudent){
+
+        this.countStudent = countStudent;
+    }
+
+    public Integer getCountTeacher(){
+
+        return countTeacher;
+    }
+
+    public TeacherList getTeacherList(){
+
+        return teacherList;
+    }
+
+    public void setTeacherList(TeacherList teacherList){
+
+        this.teacherList = teacherList;
+    }
+    
+    public void show(){
         
-        public String getAddress(){
-            
-            return address;
-        }
-        
-        public void setAddress(String address){
-            
-            this.address = address;
-        }
-        
-        public Integer getCountStudent(){
-            
-            return countStudent;
-        }
-        
-        public void setCountStudent(Integer countStudent){
-            
-            this.countStudent = countStudent;
-        }
-        
-        public Integer getCountTeacher(){
-            
-            return countTeacher;
-        }
-        
-        public void setCountTeacher(){
-            
-            countTeacher = teacherList.size();
-        }
-        
-        public ArrayList<Teacher> getTeacherList(){
-            
-            return teacherList;
-        }
-        
-        public void setTeacherList(ArrayList<Teacher> teacherList){
-            
-            this.teacherList = teacherList;
-        }
+        System.out.println(name + "\n" +
+            "ID: " + ID + "\n" +
+            "Address: " + address + "\n" +
+            "Nuber of Student: " + countStudent + "\n" +
+            "Number of Teacher: " + countTeacher + "\n" +
+            "List of teacher:");
+        teacherList.show();
+    }
 }
