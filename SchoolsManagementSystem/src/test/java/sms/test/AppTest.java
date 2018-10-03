356dba7561ffedfb1bad50becf3bb16e6d10c8ab
lekha0106 <lekha010698@gmail.com>
2018-10-03 21:49:02 +0700

    update Unit Test

---
+++ SchoolsManagementSystem/src/test/java/sms/test/AppTest.java
@@ -0,0 +1,154 @@
+package sms.test;
+
+import java.awt.font.NumericShaper;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import sms.function.Management;
+import sms.model.School;
+import sms.model.Teacher;
+import sms.view.MainView;
+import sms.view.SchoolView;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest extends TestCase {
+	
+	private static Management management = new Management();
+	private static MainView mainview = new MainView();
+	private static SchoolView schoolview = new SchoolView();
+	
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    
+    public void testSchoolManagementSystem() {
+    	assertTrue(true);
+    }
+    
+    /*
+     * Test Management Class
+     */
+    // test load database of school list
+    public void testLoadDatabaseOfSchool(){
+    	List<School> schools = new ArrayList<School>();
+    	String fileName = "truong.txt";
+    	management.loadDatabaseOfSchool(fileName, schools);
+    	
+    	// compare with number of student in database
+    	assertEquals(2, schools.size());
+    }
+    
+    // test load database of school list
+    public void testLoadDatabaseOfTeacher(){
+    	School school = new School();
+    	school.setTeachers(new ArrayList<Teacher>());
+    	String fileName = "giaovien.txt";
+    	management.loadDatabaseOfTeacher(fileName, school);
+    	
+    	// compare with number of teacher in database
+    	assertEquals(2, school.getTeachers().size());
+    }
+    
+    // test add new school
+    public void testAddSchool() {
+    	List<School> schools = new ArrayList<School>();
+    	School school = new School();
+    	management.addSchool(schools, school);
+    	
+    	assertEquals(1, schools.size());
+    }
+    
+    // test Sign a Contract With Teacher
+    public void testSignContractWithTeacher() {
+    	School school = new School();
+    	school.setTeachers(new ArrayList<Teacher>());
+    	Teacher teacher = new Teacher();
+    	management.signContractWithTeacher(school, teacher);
+    	
+    	assertEquals(1, school.getTeachers().size());
+    }
+    
+    // test search school by id 
+    public void testSearchSchoolById() {
+    	List<School> schools = new ArrayList<School>();
+    	School school = new School();
+    	school.setId("E1");
+    	schools.add(school);
+    	
+    	assertEquals(schools.get(0), management.searchSchoolById(schools, "E1"));
+    }
+    
+    // test search school by name
+    public void testSearchTeacherByName() {
+    	List<Teacher> teachers = new ArrayList<Teacher>();
+    	teachers.add(new Teacher());
+    	teachers.get(0).setName("John Carter");
+    	
+    	assertEquals(teachers.get(0), 
+    			management.searchTeacherByName(teachers, "John Carter"));
+    }
+    
+    // test search school by address
+    public void testSearchTeacherByAddress() {
+    	List<Teacher> teachers = new ArrayList<Teacher>();
+    	teachers.add(new Teacher());
+    	teachers.get(0).setAddress("Ho Chi Minh");
+    	
+    	assertEquals(teachers.get(0), 
+    			management.searchTeacherByAddress(teachers, "Ho Chi Minh"));
+    }
+    
+    
+    /*
+     * Test MainView Class
+     */
+    // test show the information of school
+    public void testShowInformationOfSchool() {
+    	School school = new School("AA", "BlaBla", 1000, "BB", 0, null);
+    	String exp = "============== BlaBla ==============\n"
+    			  +  "- Id: AA\n"
+    			  +  "- Address: BB\n"
+    			  +  "- Number of students: 1000\n"
+    			  +  "- Number of teachers: 0\n";
+    	
+    	assertEquals(exp, school.toString());
+    }
+    
+    
+    /*
+     * Test Teacher View
+     */
+    // test show the information of school
+    public void testShowInformationOfTeacher() {
+    	Teacher teacher = new Teacher(123456789, "AA", "BB");
+    	String exp = "- AA\n"
+    		      +  "- BB\n"
+    		      +  "- 123456789";
+    	
+    	assertEquals(exp, teacher.toString());
+    }
+}
