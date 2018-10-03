6a8a024df61bfee7c4c635dc340b600ccdc99b3c
lekha0106 <lekha010698@gmail.com>
2018-10-03 21:48:23 +0700

    Edit Menu And Add More Choice

---
+++ SchoolsManagementSystem/src/main/java/sms/main/SchoolManagementSystem.java
@@ -0,0 +1,114 @@
+package sms.main;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import sms.function.*;
+import sms.model.*;
+import sms.view.*;
+
+public class SchoolManagementSystem {
+
+	public static void main(String[] args) {
+		List<School> schools= new ArrayList<School>();
+		Management management = new Management();
+		Scanner scanner = new Scanner(System.in);
+		boolean stopProgram=true;
+		int choice;
+		
+		do {
+			MainView.showMainMenu();
+			choice = MainView.getMainOption(scanner);
+			System.out.println();
+			
+			switch (choice) {
+				case 1: viewAllSchool(schools); break;
+				case 2: addNewSchool(schools, scanner, management); break;
+				case 3: addNewSchoolListFromFile(schools, scanner, management); break;
+				case 4: manageTeachersList(schools, scanner, management); break;
+				case 5: stopProgram=false; break;
+			}
+		} while(stopProgram);
+
+	}
+
+	
+	// Show information of all school in list
+	public static void viewAllSchool(List<School> schools) {
+		SchoolView.showAllSchool(schools);
+	}
+
+	// add new school into list school
+	public static void addNewSchool(List<School> schools, Scanner scanner, Management management) {
+		School school = new School();
+		SchoolView.enterInformationOfSchool(school,scanner);
+		management.addSchool(schools, school);
+	}
+
+	
+	// add the school list from file name
+	public static void addNewSchoolListFromFile(List<School> schools, Scanner scanner, Management management) {
+		SchoolView.enterFileNameOfSchool();
+		management.loadDatabaseOfSchool(scanner.nextLine(), schools);
+		MainView.confirmLoadingFile();
+	}
+	
+	// manage Teacher list
+	public static void manageTeachersList(List<School> schools, Scanner scanner, Management management) {
+		boolean backToMainMenu = true;
+		
+		viewAllSchool(schools);
+		
+		SchoolView.enterSchoolId();
+		School school = management.searchSchoolById(schools, scanner.nextLine().toUpperCase());
+		if (school == null) {
+			SchoolView.showSchoolNotFound();
+			return;
+		} else {
+			do {
+				MainView.showTeacherManagementMenu(school);
+				int choice = MainView.getTeacherManagementOption(scanner);
+				System.out.println();
+				switch (choice) {
+					case 1: TeacherView.showAllTeachers(school); break;
+					case 2: {
+						if(school.getTeachers()==null) school.setTeachers(new ArrayList<Teacher>());
+						Teacher teacher = new Teacher();
+						TeacherView.enterInformationOfTeacher(teacher, scanner);
+						management.signContractWithTeacher(school, teacher);
+						break;
+					}
+					case 3: {
+						if(school.getTeachers()==null) school.setTeachers(new ArrayList<Teacher>());
+						TeacherView.enterFileNameOfTeacher();
+						management.loadDatabaseOfTeacher(scanner.nextLine(), school);
+						MainView.confirmLoadingFile();
+						break;
+					}
+					case 4: {
+						if(school.getTeachers()==null) TeacherView.showMessageEmptyTeacherList();
+						else {
+							TeacherView.enterTeacherName();
+							Teacher teacher = management.searchTeacherByName(school.getTeachers(), scanner.nextLine());
+							if (teacher == null) TeacherView.showTeacherNotFound();
+							else TeacherView.showInformationOfTeacher(teacher);
+						}
+						break;
+					}
+					case 5: {
+						if(school.getTeachers()==null) TeacherView.showMessageEmptyTeacherList();
+						else {
+							TeacherView.enterTeacherAddress();
+							Teacher teacher = management.searchTeacherByAddress(school.getTeachers(), scanner.nextLine());
+							if (teacher == null) TeacherView.showTeacherNotFound();
+							else TeacherView.showInformationOfTeacher(teacher);
+						}
+						break;
+					}
+					case 6: backToMainMenu=false;
+				}
+			}while(backToMainMenu);
+		}
+	}
+}
---
+++ SchoolsManagementSystem/src/main/java/sms/view/MainView.java
@@ -0,0 +1,41 @@
+package sms.view;
+
+import sms.model.*;
+import java.util.Scanner;
+
+public class MainView {
+	public static void showMainMenu() {
+		System.out.println("\n========== SCHOOL MANAGEMENT SYSTEM ==========");
+		System.out.println("|   1. View All Schools                      |");
+		System.out.println("|   2. Add A New School                      |");
+		System.out.println("|   3. Add School List From File             |");
+		System.out.println("|   4. Manage Teacher Of School              |");
+		System.out.println("|   5. Exit                                  |");
+		System.out.println("==============================================");
+		System.out.print("Please Enter Your Choice: ");
+	}
+
+	public static void showTeacherManagementMenu(School school) {
+		System.out.println("========= Welcome to " + school.getName().toUpperCase() + " school! =========");
+		System.out.println("|   1. View All Teachers                ");
+		System.out.println("|   2. Sign Contract With Teacher       ");
+		System.out.println("|   3. Sign Contract With Teacher List From File    ");
+		System.out.println("|   4. Search Teacher By Name           ");
+		System.out.println("|   5. Search Teacher By Address        ");
+		System.out.println("|   6. Back To Main Menu                ");
+		System.out.println("========================================");
+		System.out.print("Please Enter Your Choice: ");
+	}
+
+	public static int getMainOption(Scanner scanner) {
+		return Integer.parseInt(scanner.nextLine());
+	}
+	
+	public static int getTeacherManagementOption(Scanner scanner) {
+		return Integer.parseInt(scanner.nextLine());
+	}
+	
+	public static void confirmLoadingFile() {
+		System.out.println("Load successfull !");
+	}
+}
