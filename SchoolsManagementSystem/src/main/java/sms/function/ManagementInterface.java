a53cabb2091b6005a2ac90326432e0915e109e84
lekha0106 <lekha010698@gmail.com>
2018-10-03 21:47:04 +0700

    Add Load Data Function

---
+++ SchoolsManagementSystem/src/main/java/sms/function/ManagementInterface.java
@@ -0,0 +1,14 @@
+package sms.function;
+
+import java.util.List;
+import sms.model.*;
+
+public interface ManagementInterface {
+	public void loadDatabaseOfSchool(String fileName, List<School> schools);
+	public void loadDatabaseOfTeacher(String fileName, School schools);
+	public void addSchool(List<School> schools, School school);
+	public void signContractWithTeacher(School school, Teacher teacher);
+	public School searchSchoolById(List<School> schools,String name);
+	public Teacher searchTeacherByName(List<Teacher> teachers, String name);
+	public Teacher searchTeacherByAddress(List<Teacher> teachers, String address);
+}
