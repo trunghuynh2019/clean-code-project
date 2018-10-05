278f76a25ce1f5862bd41645995ea883512af23e
Nhat Tran <nhatb1400710@student.ctu.edu.vn>
2018-10-05 20:52:12 +0700

    Complete function Update Teacher Information

--- School Management Application/src/Service/TeacherService.java
+++ School Management Application/src/Service/TeacherService.java
@@ -29,7 +29,14 @@ public class TeacherService {
     }
     
     public Teacher updateTeacherInfor(TeacherDto teacherDto){
+        Teacher teacher = teachRepo.findByTeacherID(teacherDto.getTeacherID());
         
-        
+        teacher.setTeacherID(teacherDto.getTeacherID());
+        teacher.setSchoolID(teacherDto.getSchoolID());
+        teacher.setName(teacherDto.getName());
+        teacher.setAddress(teacherDto.getAddress());
+        teacher.setPhoneNo(teacherDto.getPhoneNo());
+        teachRepo.save(teacher);
+        return teacher;
     }
 }
