6bc403a0987b4f89ba3a9d466ea6b231455dda2a
tranxuanyen.YenTran <tranxuanyen.tranxuanyen83@gmai.com>
2018-10-08 00:39:20 +0700

    adding file of unit test

--- SchoolManagement/src/UnitTest/EMTest.java
+++ SchoolManagement/src/UnitTest/EMTest.java
@@ -1,26 +1,55 @@
 package UnitTest;
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 import org.junit.jupiter.api.Test;
 
+import sm.controller.SchoolFunction;
+import sm.controller.TeacherFunction;
 import sm.entity.School;
 import sm.entity.Teacher;
 
 class EMTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+	//void test() {
+	//	fail("Not yet implemented");
+	//}
 	
-	public void signContractWithTeacher()
+	public void testWritingFileTeacher()
 	{
+		TeacherFunction teacherFunction = new TeacherFunction();
+		List<Teacher> teachers = new ArrayList<>();
 		School school = new School();
-		assertEquals(0, school.getNumOfTeachers());
+		teacherFunction.WritingFileToTeachers(teachers);
+		assertNotNull(teachers);
 	}
 	
-
+	public void testWritingFileSchool()
+	{
+		SchoolFunction schoolFunction = new SchoolFunction();
+		List<School> schools = new ArrayList<>();
+		schoolFunction.WritingFileToSchools(schools);
+		assertNotNull(schools);
+	}
+	public void testInsertSchools()
+	{
+		SchoolFunction schoolFunction = new SchoolFunction();
+		List<School> schools = new ArrayList<>();
+		schoolFunction.insertSchools();
+		assertNotNull(schools);
+	}
+	public void testsignContractWithTeacher()
+	{
+		TeacherFunction teacherFunction = new TeacherFunction();
+		List<Teacher> teachers = new ArrayList<>();
+		School school = new School();
+		Scanner input = new Scanner(System.in);
+		teacherFunction.signContractWithTeacher(input, school.getId(), teachers);
+		assertNotNull(teachers);
+	}
 }
